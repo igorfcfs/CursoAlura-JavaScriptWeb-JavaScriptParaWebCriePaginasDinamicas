320902125480ce3497a2e04a0c643008180420e5
igorfcfs <igorfcfs@gmail.com>
2023-12-14 11:39:58 -0300

    Funções

--- aluramidi/script.js
+++ aluramidi/script.js
@@ -1,5 +1,5 @@
-// alert('Olá mundo');
+function tocaSomPom() {
+    document.querySelector('#som_tecla_pom').play();
+}
 
-// document.querySelector('button');
-document.querySelector('.tecla_pom');
-// document.querySelector('#tecla_pom');
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
