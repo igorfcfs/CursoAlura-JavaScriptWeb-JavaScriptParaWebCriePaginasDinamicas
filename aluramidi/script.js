55e5f4accea57dad8cefb289db41835a383e3fa7
igorfcfs <igorfcfs@gmail.com>
2023-12-14 12:00:19 -0300

    Listas

--- aluramidi/script.js
+++ aluramidi/script.js
@@ -2,4 +2,16 @@ function tocaSomPom() {
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+// document.querySelector('.tecla_pom').onclick = tocaSomPom;
+
+const listaDeTeclas = document.querySelectorAll('.tecla').onclick;
+
+listaDeTeclas[1].onclick = tocaSomPom;
+// listaDeTeclas[2].onclick = tocaSomPom;
+// listaDeTeclas[3].onclick = tocaSomPom;
+// listaDeTeclas[4].onclick = tocaSomPom;
+// listaDeTeclas[5].onclick = tocaSomPom;
+// listaDeTeclas[6].onclick = tocaSomPom;
+// listaDeTeclas[7].onclick = tocaSomPom;
+// listaDeTeclas[8].onclick = tocaSomPom;
+// listaDeTeclas[9].onclick = tocaSomPom;
